d2ebb3dce3a500478f789f92b6f260c4aba939fa
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-01-14 07:26:26 +0200

    css header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,8 @@ import { FunctionComponent } from "react";
 import { Container } from "../Container";
 import { Wrapper } from "../Wrapper/Wrapper";
 import './Header.scss';
+import Logo from '../../images/logo.svg'
+import Button from "../../UI/Button/Button";
 
 interface HeaderProps {
   
@@ -12,7 +14,21 @@ export const Header: FunctionComponent<HeaderProps> = () => {
     <header className="Header">
       <Container>
         <Wrapper>
-          
+          <div className="Header__container">
+            <div className="Header__logo">
+              <img src={Logo} alt="Logo" />
+            </div>
+
+            <div className="Header__nav">
+              <Button
+                className="Header__button"
+                disabled={false}
+              >
+                Users
+              </Button>
+              <Button className="Header__button">Sign in</Button>
+            </div>
+          </div>
         </Wrapper>
       </Container>
     </header>
