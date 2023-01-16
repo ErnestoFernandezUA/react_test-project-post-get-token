4b12b909e60c2b84bb619fc166e50d4939d619d0
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-01-16 10:59:27 +0200

    add: card content overflow with 3 dots

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -12,17 +12,13 @@ export const Card: FunctionComponent<UserProps> = ({ user: {
   },
   maxWidthContent,
 }) => {
-  const card = document.getElementById(`card-${id}`);
-  console.log('card', card, card?.offsetWidth ? card?.offsetWidth : 0);
-
   const style = {
-    // maxWidth: `${widthContentColumns()}px`,
     maxWidth: maxWidthContent,
     border: '1px solid transparent',
   }
 
   return (
-    <div className="Card" id={`card-${id}`}> 
+    <div className="Card"> 
       <img className="Card__photo" src={photo} alt={photo} />
       <p className="Card__name" style={style}>{name}</p>
       <p className="Card__position">{position}</p>
